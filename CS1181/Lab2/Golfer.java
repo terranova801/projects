60991e4ce47db8a05ac040829abeba1c94baa46a
Alex_Feyh <alexfeyh@gmail.com>
2025-09-10 12:08:32 -0400

    Finished implementing part B of Lab 2.
    Program now can sort golfers by score; lowest ranked higher, then number
    of holes completed, highest ranked higher, then last name, then first name.

--- CS1181/Lab2/Golfer.java
+++ CS1181/Lab2/Golfer.java
@@ -1,26 +1,72 @@
-public class Golfer {
+
+public class Golfer implements Comparable<Golfer> {
     private String firstName;
     private String lastName;
     private int score;
     private int holesCompleted;
 
-    public Golfer() {
+    public Golfer() { // default constructor
         this.firstName = "Bob";
         this.lastName = "Bill";
-        this.score = 0;
+        this.score = 1000;
         this.holesCompleted = 0;
     }
 
-    public Golfer(String firstName, String lastName, int score, int holes) {
+    public Golfer(String firstName, String lastName, int score, int holes) { // parameterized constructor
         this.firstName = firstName;
         this.lastName = lastName;
         this.score = score;
         this.holesCompleted = holes;
+    }
+
+    // compareTo method for sorting golfers
+    @Override
+    public int compareTo(Golfer other) {
+        if (Integer.compare(this.score, other.score) != 0) { // sorts by score first
+            return Integer.compare(this.score, other.score);
+        } else if (Integer.compare(this.holesCompleted, other.holesCompleted) != 0) { // sorts by holes completed next
+            return Integer.compare(this.holesCompleted, other.holesCompleted);
+        } else if (this.lastName.compareTo(other.lastName) != 0) { // sorts by last name next}
+            return this.lastName.compareTo(other.lastName);
+        } else {
+            return this.firstName.compareTo(other.firstName); // finally sorts by first name
         }
 
-    public String toString() {
-       String output = (this.lastName + ", " + this.firstName + ": " + this.score + " with " + this.holesCompleted + " holes completed");
-       return output;
     }
-    
+    // did not function as intended
+    // public int compareTo2(Golfer other) {
+    // return Integer.compare(this.holesCompleted, other.holesCompleted);
+    // }
+    // public int compareTo3(Golfer other) {
+    // return this.lastName.compareTo(other.lastName);
+    // }
+    // public int compareTo4(Golfer other) {
+    // return this.firstName.compareTo(other.firstName);
+    // }
+
+    // getters
+    public int getScore() {
+        return this.score;
+    }
+
+    public int getHolesCompleted() {
+        return this.holesCompleted;
+    }
+
+    public String getfirstName() {
+        return this.firstName;
+    }
+
+    public String getlastName() {
+        return this.lastName;
+    }
+
+    // toString method
+    public String toString() {
+        String output = (this.lastName + ", " + this.firstName + ": " + this.score + " with " + this.holesCompleted
+                + " holes completed");
+        return output;
+
+    }
+
 }
