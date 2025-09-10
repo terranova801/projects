e364e52671f6d26eca7323d6df46467eee044ad2
Alex_Feyh <alexfeyh@gmail.com>
2025-09-10 12:58:51 -0400

    Finished framework of all three classes, imported some java methods.

--- CS1181/Project_1/Chromosome.java
+++ CS1181/Project_1/Chromosome.java
@@ -1,7 +1,42 @@
 package CS1181.Project_1;
 
 import java.util.ArrayList;
+import java.util.random.RandomGenerator;
 
 public class Chromosome extends ArrayList<Item> implements Comparable<Chromosome> {
+
+    //RNG
+    public static Random rng;
+
+    //No-arg constructor
+    public Chromosome() {
+
+    }
+
+    //Crossover, generates a new chromosome from two intial chromosomes
+    public Chromosome crossover(Chromosome other) {
+
+    }
+    
+    //mutator
+    public void mutate() {
+
+    }
+
+    //Determine fitness
+    public int getFitness() {
+
+    }
+
+    //Compare
+    public int compareTo(Chromosome other) {
+
+    }
+
+    //String
+    public String toString() {
+        
+    }
+
     
 }
--- CS1181/Project_1/GeneticAlgorithm.java
+++ CS1181/Project_1/GeneticAlgorithm.java
@@ -1,5 +1,19 @@
 package CS1181.Project_1;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.io.*;
+
 public class GeneticAlgorithm {
-    
+    //Arraylist that reads txt file 
+    public static ArrayList<Item> readData (String filename) {
+        throws FileNotFoundException{}
+    }
+
+    //Creates and returns arraylist with objects and the attributes
+    public static ArrayList<Chromosome> initializePopulation (ArrayList<Item> items, int populationSize) {
+
+    }
+
+
 }
