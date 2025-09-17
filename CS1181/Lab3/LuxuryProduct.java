9ccb68aa92d30f9b0b3dad4ad76180083358bfc2
Alex Feyh <alexfeyh@proton.me>
2025-09-17 13:19:54 -0400

    Remove item class, finished, needs notations

--- CS1181/Lab3/LuxuryProduct.java
+++ CS1181/Lab3/LuxuryProduct.java
@@ -1,9 +1,28 @@
-public class LuxuryProduct extends Item implements Product {
-    
-    
+public class LuxuryProduct implements Product {
+    private String productName;
+    private double productPrice;
+    public final double taxRate = 0.0575;
 
     public LuxuryProduct(String name, double price) {
-        super(name, price);
+        this.productName = name;
+        this.productPrice = price;
+    }
+    
+
+    
+    @Override
+    public double getTotalPrice() {
+        double finalPrice = this.productPrice * (1 + taxRate);
+        return finalPrice;
+    }
+    @Override
+    public boolean isTaxable() {
+        return true;
+    }
+    @Override
+    public String toString() {
+        String toPrint = this.productName + " $" + String.format("%.2f", getTotalPrice()); //
+        return toPrint;
     }
 
 
--- CS1181/Lab3/NecessaryProduct.java
+++ CS1181/Lab3/NecessaryProduct.java
@@ -1,9 +1,30 @@
-public class NecessaryProduct extends Item implements Product {
-    private final boolean isTaxed = false; 
+public class NecessaryProduct implements Product {
+    private String productName;
+    private double productPrice;
 
     public NecessaryProduct(String name, double price) {
-        super(name, price);
+        this.productName = name;
+        this.productPrice = price;
     }
+
+  
+    @Override
+    public double getTotalPrice() {
+        return this.productPrice;
+    }
+    @Override
+    public boolean isTaxable() {
+        return false;
+    }
+    
+
+    @Override
+    public String toString() {
+        String toPrint = this.productName + " $" + String.format("%.2f", getTotalPrice());
+        return toPrint;
+    }
+
+
 
 
 
