9d1aeced4420e1c81547cb6c76dd4a6b5bcbb558
Alex_Feyh <alexfeyh@gmail.com>
2025-09-18 14:41:33 -0400

    Finished comments

--- CS1181/Lab3/LuxuryProduct.java
+++ CS1181/Lab3/LuxuryProduct.java
@@ -1,24 +1,38 @@
 public class LuxuryProduct implements Product {
     private String productName;
     private double productPrice;
-    public final double taxRate = 0.0575;
+    private final double taxRate = 0.0575;  //tax rate for luxury goods
 
+    //standard constructor    
     public LuxuryProduct(String name, double price) {
         this.productName = name;
         this.productPrice = price;
     }
     
 
-    
+    /** getTotalPrice method
+     * Inheritted from Product
+     * Takes in double productPrice, calculates & returns double finalPrice
+     */
     @Override
     public double getTotalPrice() {
         double finalPrice = this.productPrice * (1 + taxRate);
         return finalPrice;
     }
+
+    /** isTaxable
+     * Returns boolean for whether item is taxable
+     * 
+     */
     @Override
     public boolean isTaxable() {
         return true;
     }
+
+    /**
+     * toString
+     * Returns string to be printed for product name & final price
+     */
     @Override
     public String toString() {
         String toPrint = this.productName + " $" + String.format("%.2f", getTotalPrice()); //
--- CS1181/Lab3/Product.java
+++ CS1181/Lab3/Product.java
@@ -2,7 +2,7 @@
 
 public interface Product {
     
-    double getTotalPrice();
-    boolean isTaxable();
+    double getTotalPrice(); //method that calculates price given it is taxable and % of tax
+    boolean isTaxable(); //method that returns t or f if item is taxable
 
 }
