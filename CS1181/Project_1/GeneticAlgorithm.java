3595c373169da7d48f3614d4d197b454337454a0
Alex_Feyh <alexfeyh@gmail.com>
2025-09-22 12:20:01 -0400

    Worked on project 1
    Most of the item class if complete but need to double check

--- CS1181/Project_1/GeneticAlgorithm.java
+++ CS1181/Project_1/GeneticAlgorithm.java
@@ -2,18 +2,35 @@ package CS1181.Project_1;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.io.*;
 
 public class GeneticAlgorithm {
-    //Arraylist that reads txt file 
-    public static ArrayList<Item> readData (String filename) {
-        throws FileNotFoundException{}
+    // Arraylist that reads txt file
+    public static ArrayList<Item> readData(String filename) throws FileNotFoundException {
+        File itemsReader = new File(filename);
+        Scanner inputItemsFile = new Scanner (itemsReader);
+
+        String text;
+        while (inputItemsFile.hasNextLine()) {
+            text = inputItemsFile.next();
+            System.out.println(text);
+        }
+        inputItemsFile.close();
     }
 
-    //Creates and returns arraylist with objects and the attributes
-    public static ArrayList<Chromosome> initializePopulation (ArrayList<Item> items, int populationSize) {
+    // Creates and returns arraylist with objects and the attributes
+    public static ArrayList<Chromosome> initializePopulation(ArrayList<Item> items, int populationSize) {
 
     }
 
+    public static void main(String[] args) throws FileNotFoundException {
+        String fileNameOne = "Items.txt";
+        String fileNameTwo = "more_Items.txt"
+
+        
+        ArrayList<Item> readData(String filename) = new ArraryList<Item>;
+
+    }
 
 }
--- CS1181/Project_1/Item.java
+++ CS1181/Project_1/Item.java
@@ -1,42 +1,52 @@
 package CS1181.Project_1;
 
+
+/** Finished setting all methods needs documentation
+ * 
+ */
 public class Item {
-    //Object variables
+    // Object variables
     private final String name;
     private final double weight;
     private final int value;
     private boolean included;
-    
-    //Initializes Item's field 
-    public Item (String name, double weight, int value) {
 
+    // Initializes Item's field
+    public Item(String name, double weight, int value) {
+        this.name = name;
+        this.weight = weight;
+        this.value = value;
+        this.included = false;
     }
-
+    // Initializes this items fields to be the same as the other items, need to verify function
     public Item(Item other) {
-        
+        this.name = other.name;
+        this.weight = other.weight;
+        this.value = other.value;
+     //   this.included = other.included; //unsure if should be included
     }
 
-    
-    
-    
-    //getters
+    // getters
     public double getWeight() {
-
+        return this.weight;
     }
+
     public int getValue() {
-
+        return this.value;
     }
+
     public boolean isIncluded() {
-
+        return this.included;
     }
 
-    //setters
+    // setters
     public void setIncluded(boolean Included) {
-
+        this.included = Included;
     }
-    public String toString() {
 
+    public String toString() {
+        String toPrint = this.name + " (" + this.weight + " lbs , $" + this.value + ")";
+        return toPrint;
     }
 
 }
-
