4ace57c4abfbf5c619b19e365e908ff8f075de3e
Alex Feyh <alexfeyh@proton.me>
2025-09-22 19:42:24 -0400

    Finished lab 4b...will need javadocs

--- CS1181/Lab4/Main.java
+++ CS1181/Lab4/Main.java
@@ -1,32 +1,45 @@
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class Main {
-   
-    public static void main(String[] args) {
-        ArrayList<Athlete> athletes = new ArrayList<Athlete>();
 
-        athletes.add(new Athlete("John", "Doe", "baseball", 33));
-        athletes.add(new Athlete("James", "Shelton", "swimming", 4));
-        athletes.add(new Athlete("Sam", "Johnson", "football", 77));
-        athletes.add(new Athlete("Kevin", "Smith", "baseball", 2));
-        athletes.add(new Athlete("Sally", "Johnson", "swimming", 9));
-        athletes.add(new Athlete("James", "Smith", "swimming", 43));
-        athletes.add(new Athlete("Meagan", "Kelly", "swimming", 1));
+    public static void main(String[] args) {
+        ArrayList<Athlete<Integer>> athletes = new ArrayList<>();
+
+        athletes.add(new Athlete<>("John Doe", "baseball", 33));
+        athletes.add(new Athlete<>("James Shelton", "swimming", 4));
+        athletes.add(new Athlete<>("Sam Johnson", "football", 77));
+        athletes.add(new Athlete<>("Kevin Smith", "baseball", 2));
+        athletes.add(new Athlete<>("Sally Johnson", "swimming", 9));
+        athletes.add(new Athlete<>("James Smith", "swimming", 43));
+        athletes.add(new Athlete<>("Meagan Kelly", "swimming", 1));
 
         // Unsorted outputs
-        System.out.println("\n" + "Before Sorting:" + "\n");
-        for (Athlete a : athletes) {
+        System.out.println("\n" + "Unsorted" + "\n");
+        for (Athlete<Integer> a : athletes) {
             System.out.println(a.toString());
         }
 
         Collections.sort(athletes);
 
-        System.out.println("\n" + "After Sorting:" + "\n");
-        for (Athlete a : athletes) {
+        System.out.println("\n" + "Sorted by sport and then name" + "\n");
+        for (Athlete<Integer> a : athletes) {
             System.out.println(a.toString());
         }
+
+
+        Collections.sort(athletes, Comparator 
+        .comparing(Athlete<Integer>::getSport, String.CASE_INSENSITIVE_ORDER).thenComparing(Athlete<Integer>::getRank, Comparator.naturalOrder()));
+
+
+
+        System.out.println("\n" + "Sorted by sport and then ranking" + "\n");
+        for (Athlete<Integer> a : athletes) {
+            System.out.println(a.toString());
+        }
+        
 
     }
 }
