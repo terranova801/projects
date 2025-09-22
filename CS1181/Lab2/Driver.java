e70f4172ece0d43f08ef8c7b763ec9a31d32a0cb
Alex_Feyh <alexfeyh@gmail.com>
2025-09-22 12:46:53 -0400

    Lab 4 part a created

--- CS1181/Lab2/Driver.java
+++ CS1181/Lab2/Driver.java
@@ -1,42 +1,43 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
-
 /**
  * Driver class which functions as main method to create Golfer objects,
  * store them in an ArrayList, print the unsorted list, sort the list,
  * and print the sorted list.
+ * 
  * @author Alex Feyh
  * @version 1.0
- * lab 2
- * Citations for sources used:
- * 1. Java comparable interface documentation - https://www.geeksforgeeks.org/java/comparable-interface-in-java-with-examples/
- * 2. Java Comparable vs Comparator - https://www.baeldung.com/java-comparator-comparable
+ *          lab 2
+ *          Citations for sources used:
+ *          1. Java comparable interface documentation -
+ *          https://www.geeksforgeeks.org/java/comparable-interface-in-java-with-examples/
+ *          2. Java Comparable vs Comparator -
+ *          https://www.baeldung.com/java-comparator-comparable
  */
 public class Driver {
-    public static void main(String[] args) {  
-    ArrayList<Golfer> golfers = new ArrayList<Golfer>();
-    
-    golfers.add(new Golfer("John", "Smith", -8, 15));
-    golfers.add(new Golfer("Michael", "Brooks", -12, 15));
-    golfers.add(new Golfer("Sarah", "Adams", -10, 15));
-    golfers.add(new Golfer("Abby", "Adams", -8, 16));
-    golfers.add(new Golfer("Peter", "Collins", -8, 16));
-    golfers.add(new Golfer("Jane", "Smith", -8, 15));
-    golfers.add(new Golfer("Amy", "Daniels", -12, 14));
+    public static void main(String[] args) {
+        ArrayList<Golfer> golfers = new ArrayList<Golfer>();
 
-    System.out.println("\n" + "Before Sorting:" + "\n");
-    for (Golfer g :  golfers) {
+        golfers.add(new Golfer("John", "Smith", -8, 15));
+        golfers.add(new Golfer("Michael", "Brooks", -12, 15));
+        golfers.add(new Golfer("Sarah", "Adams", -10, 15));
+        golfers.add(new Golfer("Abby", "Adams", -8, 16));
+        golfers.add(new Golfer("Peter", "Collins", -8, 16));
+        golfers.add(new Golfer("Jane", "Smith", -8, 15));
+        golfers.add(new Golfer("Amy", "Daniels", -12, 14));
+
+        System.out.println("\n" + "Before Sorting:" + "\n");
+        for (Golfer g : golfers) {
             System.out.println(g.toString());
-    }
-    //Sort method that calls to the compareTo method in Golfer class
-    Collections.sort(golfers);
+        }
+        // Sort method that calls to the compareTo method in Golfer class
+        Collections.sort(golfers);
 
-    System.out.println("\n" + "After Sorting:" + "\n");
-    for (Golfer g :  golfers) {
-        System.out.println(g.toString());
-    }
-
+        System.out.println("\n" + "After Sorting:" + "\n");
+        for (Golfer g : golfers) {
+            System.out.println(g.toString());
+        }
 
     }
 }
---
+++ CS1181/Lab4/Main.java
@@ -0,0 +1,30 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Athlete> athletes = new ArrayList<Athlete>();
+
+        athletes.add(new Athlete("John", "Doe", "baseball"));
+        athletes.add(new Athlete("Sam", "Johnson", "football"));
+        athletes.add(new Athlete("Kevin", "Smith", "baseball"));
+        athletes.add(new Athlete("Sally", "Johnson", "swimming"));
+        athletes.add(new Athlete("James", "Smith", "swimming"));
+        athletes.add(new Athlete("Meagan", "Kelly", "swimming"));
+
+        // Unsorted outputs
+        System.out.println("\n" + "Before Sorting:" + "\n");
+        for (Athlete a : athletes) {
+            System.out.println(a.toString());
+        }
+
+        Collections.sort(athletes);
+
+        System.out.println("\n" + "After Sorting:" + "\n");
+        for (Athlete a : athletes) {
+            System.out.println(a.toString());
+        }
+
+    }
+}
