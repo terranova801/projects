c1a35b24cefc2b96f7abbf5329b2a86752180180
Alex_Feyh <alexfeyh@gmail.com>
2025-10-13 12:11:26 -0400

    Utilizing inheritance when creating a window GUI

--- CS1181Class/GraphicalUserInterfaces/src/MainWindow.java
+++ CS1181Class/GraphicalUserInterfaces/src/MainWindow.java
@@ -1,11 +1,18 @@
 import javax.swing.*;
 
 
-public class Main {
+class MainWindow extends JFrame {
+    private int clickCount;
+
+    public MainWindow(String title) {
+        super(title);
+        clickCount = 0;
+    }
+
     public static void main(String[] args) {
         
         //Creating the intial window
-        JFrame theWindow = new JFrame("Our first window!");
+        JFrame theWindow = new MainWindow("Our first window!");
         theWindow.setSize(1500,1400); //size of the window in pixels
         theWindow.setLocation(200,400); //sets location on screen
         theWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
