c5093667a06e45183da881c8fbb7d164d5be0a9e
Alex_Feyh <alexfeyh@gmail.com>
2025-10-20 12:34:27 -0400

    Auto commit: 2025-10-20 12:34:27

---
+++ CS1181/Lab7/GettingWarmer/src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame gui = new JFrame("tempConverter");
+        gui.setSize(700, 500);
+        gui.setLocation(100, 100);
+        gui.setVisible(true);
+        gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton button = new JButton("Push to Convert");
+        JLabel label = new JLabel("Convert");
+
+        JPanel panel = new JPanel();
+        panel.add(button);
+        panel.add(label);
+        gui.add(panel);
+    }
+}
