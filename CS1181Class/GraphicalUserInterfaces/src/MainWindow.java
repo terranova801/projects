c4dd71699c33f1c5eb35904c3fa59c57b1229b2f
Alex_Feyh <alexfeyh@gmail.com>
2025-10-20 12:34:27 -0400

    Auto commit: 2025-10-20 12:34:27

--- CS1181Class/GraphicalUserInterfaces/src/MainWindow.java
+++ CS1181Class/GraphicalUserInterfaces/src/MainWindow.java
@@ -1,27 +1,27 @@
-// import javax.swing.*;
+import javax.swing.*;
 
 
-// public class MainWindow extends JFrame {
+public class MainWindow extends JFrame {
    
-//     private int clickCount;
+    private int clickCount;
 
    
-//     public MainWindow(String title) {
-//         super(title);
-//         clickCount = 0;
-//     }
+    public MainWindow(String title) {
+        super(title);
+        clickCount = 0;
+    }
 
-//     public static void main(String[] args) {
+    public static void main(String[] args) {
         
-//         //Creating the intial window
-//         JFrame window = new MainWindow("Our first window!");
-//         window.setSize(1500,1400); //size of the window in pixels
-//         window.setLocation(200,400); //sets location on screen
-//         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//         window.setVisible(true);
+        //Creating the intial window
+        JFrame window = new MainWindow("Our first window!");
+        window.setSize(1500,1400); //size of the window in pixels
+        window.setLocation(200,400); //sets location on screen
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
         
-//         //Keeping track of data until the main window is closed
+        //Keeping track of data until the main window is closed
 
 
-//     }
-// }
+    }
+}
